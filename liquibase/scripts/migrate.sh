75e2003433aba041eb3b365cbb65a94c67810472
yusuke.kawata <test@inte.co.jp>
2018-02-27 20:18:42 +0900

    add migrate.sh

---
+++ liquibase/scripts/migrate.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ -z "${TARGET_CHANGE_LOG}" ]; then
+  echo "required: TARGET_CHANGE_LOG" >&2
+  exit 1
+fi
+
+if [ -z "${HOST}" ]; then
+  echo "required: HOST" >&2
+  exit 1
+fi
+
+if [ -z "${PORT}" ]; then
+  echo "required: PORT" >&2
+  exit 1
+fi
+
+if [ -z "${USER}" ]; then
+  echo "required: USER" >&2
+  exit 1
+fi
+
+if [ -z "${PASSWORD}" ]; then
+  echo "required: PASSWORD" >&2
+  exit 1
+fi
+
+if [ -z "${SCHEME}" ]; then
+  echo "required: SCHEME" >&2
+  exit 1
+fi
+
+liquibase --driver=com.mysql.jdbc.Driver \
+     --classpath=/opt/liquibase/mysql-connector-java.jar \
+     --changeLogFile=${TARGET_CHENGE_LOG} \
+     --url="jdbc:mysql://${HOST}:${PORT}/${SCHEME}" \
+     --username=${USER} \
+     --password=${PASSWORD} \
+     migrate
